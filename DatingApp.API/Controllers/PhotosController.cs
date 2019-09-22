bd008755f7f7f4726991089f336ef509122c0c41
Ivaylo Milanoc <itm932@gmail.com>
2019-09-22 09:00:58 +0300

    Photo to cloud upload functionality added

---
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IDatingRepository repo;
+        private readonly IMapper mapper;
+        private readonly IOptions<CloudinarySettings> cloudinaryConfig;
+        private Cloudinary cloudinary;
+
+        public PhotosController(IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            this.cloudinaryConfig = cloudinaryConfig;
+            this.mapper = mapper;
+            this.repo = repo;
+
+            Account acc = new Account(
+                this.cloudinaryConfig.Value.CloudName,
+                this.cloudinaryConfig.Value.ApiKey,
+                this.cloudinaryConfig.Value.ApiSecret
+            );
+
+            this.cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id)
+        {
+            var photoFromRepo = await this.repo.GetPhoto(id);
+            var photo = this.mapper.Map<PhotoForReturnDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddPhotoForUser(int UserId, [FromForm]PhotoForCreationDto photoForCreationDto)
+        {
+            if (UserId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await repo.GetUser(UserId);
+
+            var file = photoForCreationDto.File;
+            var uploadResult = new ImageUploadResult();
+            
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams
+                    {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = this.cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = this.mapper.Map<Photo>(photoForCreationDto);
+
+            if (!userFromRepo.Photos.Any(u => u.IsMain))
+                photo.IsMain = true;
+
+            userFromRepo.Photos.Add(photo);
+
+            if (await this.repo.SaveAll())
+            {
+                var photoToReturn = this.mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new {id = photo.Id}, photoToReturn);
+
+            }
+
+            return BadRequest("Could not add photo");
+        }
+    }
+}
